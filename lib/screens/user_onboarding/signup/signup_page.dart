89fcdbe2077d986edf253c39d8fd6072e685ee87
BILALAHMAD5587 <bilalmalik5587@gmail.com>
2023-02-15 10:14:09 +0500

    final

--- lib/screens/user_onboarding/signup/signup_page.dart
+++ lib/screens/user_onboarding/signup/signup_page.dart
@@ -54,145 +54,147 @@ class _SiginUp_PageState extends State<SiginUp_Page> {
       print(constraints.maxWidth);
       print(constraints.maxWidth);
 
-      return Center(child:
-        Padding(padding: const EdgeInsets.all(21),
-        child: Form(
-          key: _formkey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CustomLogoStack(width > 500 ? 50 : 34),
-              SizedBox(height: 21,),
-              Text('Get Started',
-              style: constraints.maxWidth > 500 ? mTextStylr43(
-                fontweight: FontWeight.bold,
-                mColor: Theme.of(context).canvasColor) : mTextStylr26(
-                fontweight: FontWeight.bold,
-                mColor: Theme.of(context).canvasColor),),
-              SizedBox(height: 11,),
-              Text('Start managing Your Expense Today!',
-                style: constraints.maxWidth > 500 ? mTextStylr19(
-                    fontweight: FontWeight.w300,
-                    mColor: Theme.of(context).canvasColor) : mTextStylr12(
-                    fontweight: FontWeight.w300,
-                    mColor: Theme.of(context).canvasColor),),
-              ///////////////////////////////////Uname And Password////////////////////
-                SizedBox(height: 21),
-              CustomTextFiled(
-                errorMsg: 'Please Enter Your Name',
-                  mcontroller: nameController,
-                  mfillcolor: Theme.of(context).brightness == Brightness.light
-                  ? MyColors.secondryWColor
-                  : MyColors.secoundryBColor,
-                  micon: Icons.account_circle_outlined,
-                  isPassword: false,
-                  hint: 'Name Hare...'),
-
-              SizedBox(height: 11,),
-              CustomTextFiled(
-                errorMsg: 'Please Enter Your Email',
-                  mcontroller: emailcontroller,
-                  mfillcolor: Theme.of(context).brightness == Brightness.light
-                      ? MyColors.secondryWColor
-                      : MyColors.secoundryBColor,
-                  micon: Icons.email_outlined,
-                  isPassword: false,
-                  hint: 'Email Here..'),
-              SizedBox(height: 11,),
-              CustomTextFiled(
-                errorMsg: 'Chouse Your Passeord',
-                  mcontroller: passController,
-                  mfillcolor: Theme.of(context).brightness == Brightness.light
-                      ? MyColors.secondryWColor
-                      : MyColors.secoundryBColor,
-                  micon: Icons.lock_open_outlined,
-                  isPassword: true,
-                  hint: 'Password here'),
-              SizedBox(height: 7,),
-              FlutterPwValidator(
-                  controller: passController,
-                  minLength: 6,
-                  uppercaseCharCount: 1,
-                  numericCharCount: 3,
-                  specialCharCount: 1,
-                  width: 350,
-                  height: 100,
-                  onSuccess: (){},
-                  onFail: (){}
-              ),
-              SizedBox(height: 11,),
-              CustomTextFiled(
-                  errorMsg: 'Chouse Your Confirm Passeord',
-                  mcontroller: ConfirmPassController,
-                  mfillcolor: Theme.of(context).brightness == Brightness.light
-                      ? MyColors.secondryWColor
-                      : MyColors.secoundryBColor,
-                  micon: Icons.lock_open_outlined,
-                  isPassword: true,
-                  hint: 'Confirm Password here'),
-              SizedBox(height: 7,),
-              FlutterPwValidator(
-                  controller: ConfirmPassController,
-                  minLength: 5,
-                  uppercaseCharCount: 1,
-                  numericCharCount: 3,
-                  specialCharCount: 1,
-                  width: 350,
-                  height: 100,
-                  onSuccess: (){},
-                  onFail: (){}
-              ),
-
-              SizedBox(height: 25,),
-              CustomRoundedBtn(callback: (){
-
-
-                if (nameController.text.toString().isNotEmpty
-                    && passController.text.toString().isNotEmpty
-                && emailcontroller.text.toString().isNotEmpty
-                 && passController.text.toString().isNotEmpty
-                && ConfirmPassController.text.toString().isNotEmpty){
-                  //login process
-                } else {
-                  showDialog(context: context, builder: (context){
-                    return AlertDialog(
-                      title: Text('Stop'),
-                      content: Text('Please Fill all the required blanks!!'),
-                    );
-                  });
-                }
-
-
-
-                if (_formkey.currentState!.validate()) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('Processing Data'))
-                  );
-                }
-              }, text: 'Sign Up'),
-              SizedBox(height: 11,),
-              if (constraints.maxWidth > 400 )
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: getChildren('Already have an Account, ', 'Login Now', (){
-                    Navigator.pushReplacement(context,
-                        MaterialPageRoute(builder:
-                            (context) =>  LoginPage(),
-                        ));}, width, context)
-                )
-      else Column(
+      return SafeArea(
+        child: SingleChildScrollView(
+          child: Center(child:
+            Padding(padding: const EdgeInsets.all(21),
+            child: Form(
+              key: _formkey,
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: getChildren('Already have an Account, ', 'Login now', (){
-                  Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (context) => LoginPage(),));
-                }, width, context),
-              )
+                children: [
+                  CustomLogoStack(width > 500 ? 50 : 34),
+                  SizedBox(height: 21,),
+                  Text('Get Started',
+                  style: constraints.maxWidth > 500 ? mTextStylr43(
+                    fontweight: FontWeight.bold,
+                    mColor: Theme.of(context).canvasColor) : mTextStylr26(
+                    fontweight: FontWeight.bold,
+                    mColor: Theme.of(context).canvasColor),),
+                  SizedBox(height: 11,),
+                  Text('Start managing Your Expense Today!',
+                    style: constraints.maxWidth > 500 ? mTextStylr19(
+                        fontweight: FontWeight.w300,
+                        mColor: Theme.of(context).canvasColor) : mTextStylr12(
+                        fontweight: FontWeight.w300,
+                        mColor: Theme.of(context).canvasColor),),
+                  ///////////////////////////////////Uname And Password////////////////////
+                    SizedBox(height: 21),
+                  CustomTextFiled(
+                    errorMsg: 'Please Enter Your Name',
+                      mcontroller: nameController,
+                      mfillcolor: Theme.of(context).brightness == Brightness.light
+                      ? MyColors.secondryWColor
+                      : MyColors.secoundryBColor,
+                      micon: Icons.account_circle_outlined,
+                      isPassword: false,
+                      hint: 'Name Hare...'),
+
+                  SizedBox(height: 11,),
+                  CustomTextFiled(
+                    errorMsg: 'Please Enter Your Email',
+                      mcontroller: emailcontroller,
+                      mfillcolor: Theme.of(context).brightness == Brightness.light
+                          ? MyColors.secondryWColor
+                          : MyColors.secoundryBColor,
+                      micon: Icons.email_outlined,
+                      isPassword: false,
+                      hint: 'Email Here..'),
+                  SizedBox(height: 11,),
+                  CustomTextFiled(
+                    errorMsg: 'Chouse Your Passeord',
+                      mcontroller: passController,
+                      mfillcolor: Theme.of(context).brightness == Brightness.light
+                          ? MyColors.secondryWColor
+                          : MyColors.secoundryBColor,
+                      micon: Icons.lock_open_outlined,
+                      isPassword: true,
+                      hint: 'Password here'),
+                 FlutterPwValidator(
+                      controller: passController,
+                      minLength: 6,
+                      uppercaseCharCount: 1,
+                      numericCharCount: 3,
+                      specialCharCount: 1,
+                      width: 300,
+                      height: 95,
+                      onSuccess: (){},
+                      onFail: (){}
+                  ),
+                  SizedBox(height: 11,),
+                  CustomTextFiled(
+                      errorMsg: 'Chouse Your Confirm Passeord',
+                      mcontroller: ConfirmPassController,
+                      mfillcolor: Theme.of(context).brightness == Brightness.light
+                          ? MyColors.secondryWColor
+                          : MyColors.secoundryBColor,
+                      micon: Icons.lock_open_outlined,
+                      isPassword: true,
+                      hint: 'Confirm Password here'),
+                  /*FlutterPwValidator(
+                      controller: ConfirmPassController,
+                      minLength: 5,
+                      uppercaseCharCount: 1,
+                      numericCharCount: 3,
+                      specialCharCount: 1,
+                      width: 350,
+                      height: 100,
+                      onSuccess: (){},
+                      onFail: (){}
+                  ),*/
+
+                  SizedBox(height: 25,),
+                  CustomRoundedBtn(callback: (){
 
 
-            ],
+                    if (nameController.text.toString().isNotEmpty
+                        && passController.text.toString().isNotEmpty
+                    && emailcontroller.text.toString().isNotEmpty
+                     && passController.text.toString().isNotEmpty
+                    && ConfirmPassController.text.toString().isNotEmpty){
+                      //login process
+                    } else {
+                      showDialog(context: context, builder: (context){
+                        return AlertDialog(
+                          title: Text('Stop'),
+                          content: Text('Please Fill all the required blanks!!'),
+                        );
+                      });
+                    }
+
+
+
+                    if (_formkey.currentState!.validate()) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Processing Data'))
+                      );
+                    }
+                  }, text: 'Sign Up'),
+                  SizedBox(height: 11,),
+                  if (constraints.maxWidth > 400 )
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: getChildren('Already have an Account, ', 'Login Now', (){
+                        Navigator.pushReplacement(context,
+                            MaterialPageRoute(builder:
+                                (context) =>  LoginPage(),
+                            ));}, width, context)
+                    )
+          else Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: getChildren('Already have an Account, ', 'Login now', (){
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context) => LoginPage(),));
+                    }, width, context),
+                  )
+
+
+                ],
+              ),
+            ),
+            ),
           ),
-        ),
         ),
       );
     }
