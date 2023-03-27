0245122fa3e67dd5245c370261802417383bd087
BILALAHMAD5587 <bilalmalik5587@gmail.com>
2023-03-27 11:14:08 +0500

    home Page Adedd

--- lib/screens/home/frags/frag_transaction/frag_tranaction.dart
+++ lib/screens/home/frags/frag_transaction/frag_tranaction.dart
@@ -12,77 +12,32 @@ class FragTransaction extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
           backgroundColor: Theme.of(context).backgroundColor,
-          body: Column(
-            children: [
-              Expanded(
-                  flex: 1,
-                  child: Container(
-                    width: double.infinity,
-                    child: Padding(
-                      padding: const EdgeInsets.all(3.0),
-                      child: Align(
-                        alignment: Alignment.centerRight,
-                        child: CircleAvatar(
-                          backgroundColor:
-                              isLight ? MyColors.bgBColor : MyColors.bgWColor,
-                          child: Icon(
-                            Icons.add,
-                            color:
-                                isLight ? MyColors.bgWColor : MyColors.bgBColor,
-                            size: 21,
-                          ),
-                        ),
-                      ),
-                    ),
-                  )),
-              Expanded(
-                  flex: 7,
-                  child: Container(
-                    //color: Colors.green.shade200,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text('Spent this week',
-                            style: mTextStylr12(
-                                mColor: isLight
-                                    ? MyColors.secoundryBColor
-                                    : MyColors.secondryWColor)),
-                        RichText(
-                            text: TextSpan(children: [
-                          TextSpan(
-                              text: '\$ ',
-                              style: mTextStylr26(
-                                  mColor: isLight
-                                  ? MyColors.secoundryBColor
-                                  : MyColors.secondryWColor,
-                                  fontweight: FontWeight.bold)),
-                          TextSpan(
-                              text: '9999',
-                              style: mTextStylr52(
-                                  mColor: isLight
-                                  ? MyColors.secoundryBColor
-                                  : MyColors.secondryWColor,
-                                  fontweight: FontWeight.bold)),
-                          TextSpan(
-                              text: '.50',
-                              style: mTextStylr26(mColor: isLight
-                              ? MyColors.secoundryBColor
-                              : MyColors.secondryWColor))
-                        ]))
-                      ],
-                    ),
-                  )),
-              Expanded(
-                  flex: 11,
-                  child: ListView.builder(
-                    itemCount: Constants.arrTransaction.length,
-                    itemBuilder: (context, index) =>
-                        dayWiseTrasanctionItem(Constants.arrTransaction[index]),
-                  )),
-            ],
-          )),
+          body: portraitUI()),
     );
   }
+
+  Widget landscapeUI(){
+    return Container();
+  }
+
+
+  Widget portraitUI(){
+    return Column(
+      children: [
+        Expanded(
+            flex: 1,
+            child: addTransactionUI()),
+        Expanded(
+            flex: 7,
+            child: totalBalanceUI()),
+        Expanded(
+            flex: 11,
+            child: allTransactionUI()),
+      ],
+    );
+  }
+
+
 
   Widget dayWiseTrasanctionItem(Map dayTransDetails) {
     return Padding(
@@ -159,4 +114,73 @@ class FragTransaction extends StatelessWidget {
       ),
     );
   }
+
+  Widget addTransactionUI(){
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.all(3.0),
+        child: Align(
+          alignment: Alignment.centerRight,
+          child: CircleAvatar(
+            backgroundColor:
+            isLight ? MyColors.bgBColor : MyColors.bgWColor,
+            child: Icon(
+              Icons.add,
+              color:
+              isLight ? MyColors.bgWColor : MyColors.bgBColor,
+              size: 21,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget totalBalanceUI(){
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text('Spent this week',
+            style: mTextStylr12(
+                mColor: isLight
+                    ? MyColors.secoundryBColor
+                    : MyColors.secondryWColor)),
+        RichText(
+            text: TextSpan(children: [
+              TextSpan(
+                  text: '\$ ',
+                  style: mTextStylr26(
+                      mColor: isLight
+                          ? MyColors.secoundryBColor
+                          : MyColors.secondryWColor,
+                      fontweight: FontWeight.bold)),
+              TextSpan(
+                  text: '9999',
+                  style: mTextStylr52(
+                      mColor: isLight
+                          ? MyColors.secoundryBColor
+                          : MyColors.secondryWColor,
+                      fontweight: FontWeight.bold)),
+              TextSpan(
+                  text: '.50',
+                  style: mTextStylr26(mColor: isLight
+                      ? MyColors.secoundryBColor
+                      : MyColors.secondryWColor))
+            ]))
+      ],
+    );
+  }
+
+  Widget allTransactionUI(){
+    return ListView.builder(
+      itemCount: Constants.arrTransaction.length,
+      itemBuilder: (context, index) =>
+          dayWiseTrasanctionItem(Constants.arrTransaction[index]),
+    );
+  }
+
+
+
 }
